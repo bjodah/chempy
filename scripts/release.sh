f5f519a4b1bdd3bf26a46f40ad5dde48a7dbef2f
Björn Dahlgren <bjodah@gmail.com>
2016-02-11 19:35:18 +0100

    Tweak build script, bump dev version.

--- scripts/release.sh
+++ scripts/release.sh
@@ -17,19 +17,22 @@ PKG_UPPER=$(echo $PKG | tr '[:lower:]' '[:upper:]')
 env ${PKG_UPPER}_RELEASE_VERSION=$1 python setup.py sdist
 env ${PKG_UPPER}_RELEASE_VERSION=$1 ./scripts/generate_docs.sh
 PATH=$2:$PATH ./scripts/build_conda_recipe.sh $1
-# All went well
+
+# All went well, add a tag and push it.
 git tag -a $1 -m $1
 git push
 git push --tags
 VERSION=${1#v}
 twine upload dist/${PKG}-$VERSION.tar.gz
-MD5=$(md5sum dist/${PKG}-${1#v}.tar.gz | cut -f1 -d' ')
+MD5=$(md5sum dist/${PKG}-$VERSION.tar.gz | cut -f1 -d' ')
 cp -r conda-recipe/ dist/conda-recipe-${1#v}
 sed -i -E -e "s/version:(.+)/version: $VERSION/" -e "s/path:(.+)/fn: $PKG-$VERSION.tar.gz\n    url: https:\/\/pypi.python.org\/packages\/source\/${PKG:0:1}\/$PKG\/$PKG-$VERSION.tar.gz#md5=$MD5\n    md5: $MD5/" dist/conda-recipe-${1#v}/meta.yaml
 env ${PKG_UPPER}_RELEASE_VERSION=$1 python setup.py upload_sphinx
 
 # Specific for this project:
-scp -r dist/conda-recipe-${1#v}/ $PKG@hera:~
-ssh $PKG@hera "conda-build conda-recipe-${1#v}/"
+SERVER=hera
+scp -r dist/conda-recipe-${1#v}/ $PKG@$SERVER:~/public_html/conda-recipes/
+scp dist/${PKG}-$VERSION.tar.gz $PKG@$SERVER:~/public_html/releases/
+ssh $PKG@$SERVER "source /etc/profile; conda-build conda-recipe-${1#v}/"
 
 echo "Remember to build conda-recipe, bump version (and commit and push)!"
