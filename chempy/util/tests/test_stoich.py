fd0130b8fcfc2056ce4c275256ff24b34b7f9dcf
Björn Dahlgren <bjodah@gmail.com>
2016-04-20 11:54:41 +0200

    Handle units in .util.stoich.decompose_yields

--- chempy/util/tests/test_stoich.py
+++ chempy/util/tests/test_stoich.py
@@ -9,7 +9,7 @@ else:
     from ..stoich import (
         get_coeff_mtx, decompose_yields,
     )
-
+from chempy.units import units_library, allclose, _sum
 from ..testing import requires
 
 
@@ -61,6 +61,41 @@ def test_decompose_yields_1():
                 i, rxn in enumerate(rxns))
 
     assert abs(G_H2O+4.64) < 1e-3
+
+
+@requires(units_library)
+def test_decompose_yields__units_1():
+    from chempy import Reaction
+    from chempy.units import default_units as u
+    gamma_yields = {
+        'OH-': 0.5*u.per100eV,
+        'H2O2': 0.7*u.per100eV,
+        'OH': 2.7*u.per100eV,
+        'H2': 0.45*u.per100eV,
+        'H': 0.66*u.per100eV,
+        'H+': 3.1*u.per100eV,
+        'HO2': 0.02*u.per100eV,
+        'e-(aq)': 2.6*u.per100eV,
+    }
+
+    rxns = [
+        Reaction({'H2O': 1}, {'H+': 1, 'OH-': 1}),
+        Reaction({'H2O': 1}, {'H+': 1, 'e-(aq)': 1, 'OH': 1}),
+        Reaction({'H2O': 1}, {'H': 2, 'H2O2': 1}, inact_reac={'H2O': 1}),
+        Reaction({'H2O': 1}, {'H2': 1, 'H2O2': 1}, inact_reac={'H2O': 1}),
+        Reaction({'H2O': 1}, {'H2': 1, 'OH': 2}, inact_reac={'H2O': 1}),
+        Reaction({'H2O': 1}, {'H2': 3, 'HO2': 2}, inact_reac={'H2O': 3}),
+    ]
+
+    k = decompose_yields(gamma_yields, rxns)
+    k_ref = [0.5, 2.6, 0.33, 0.37, 0.05, 0.01]*u.per100eV
+
+    assert allclose(k, k_ref)
+
+
+    G_H2O = [rxn.net_stoich(['H2O'])[0]*k[i] for i, rxn in enumerate(rxns)]
+    ref = 4.64*u.per100eV
+    assert abs((_sum(G_H2O)+ref)/ref) < 1e-3
 
 
 @requires('numpy')
