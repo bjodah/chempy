9024f66eb49dad45dc110956832ad6facb034131
Björn Dahlgren <bjodah@gmail.com>
2016-04-30 13:05:33 +0200

    Move .core, .debye_huckel -> .ionic_strength

--- chempy/ionic_strength.py
+++ chempy/ionic_strength.py
@@ -1,12 +1,48 @@
 # -*- coding: utf-8 -*-
 """
-This modules collects expressions for the Debye-Hückel constants.
-
+This modules collects expressions related to ionic strenght, e.g. the Debye-Hückel expressions.
 """
 from __future__ import (absolute_import, division, print_function)
 
-from .core import _ActivityProductBase, ionic_strength
 from ._util import get_backend
+
+
+def ionic_strength(molalities, charges, b0=1):
+    """ Calculates the ionic strength
+
+    Parameters
+    ----------
+    molalities: float
+        optionally with unit (amount / mass)
+    charges: iterable of integers
+        charge of respective ion
+    b0: float
+        reference molality, optionally with unit (amount / mass)
+        by IUPAC defines it as 1 mol/kg. (default: 1)
+
+    Examples
+    --------
+    >>> ionic_strength([1e-3, 3e-3], [3, -1]) == .5 * (9 + 3) * 1e-3
+    True
+
+    """
+    tot = 0
+    if len(molalities) != len(charges):
+        raise ValueError("molalities and charges of different lengths")
+    for b, z in zip(molalities, charges):
+        tot += (b/b0)*z**2
+    return tot/2
+
+
+class _ActivityProductBase(object):
+    """ Baseclass for activity products """
+
+    def __init__(self, stoich, *args):
+        self.stoich = stoich
+        self.args = args
+
+    def __call__(self, c):
+        pass
 
 
 def A(eps_r, T, rho, b0=1, constants=None, units=None, backend=None):
--- chempy/tests/test_ionic_strength.py
+++ chempy/tests/test_ionic_strength.py
@@ -1,11 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function)
+
 from math import log as ln
 
-from ..debye_huckel import A as A_dh, B as B_dh
-from ..debye_huckel import limiting_log_gamma
+from ..ionic_strength import A as A_dh, B as B_dh
+from ..ionic_strength import limiting_log_gamma, _ActivityProductBase, ionic_strength
 from ..units import (allclose, units_library,
                      default_constants as consts,
                      default_units as u)
 from ..util.testing import requires
+
+
+def test_ionic_strength():
+    assert abs(ionic_strength([0.1, 1.3, 2.1, 0.7],
+                              [-1, 2, -3, 4]) - 17.7) < 1e-14
+
+
+def test_ActivityProductBase():
+    ap = _ActivityProductBase((1, -2, 3), 17, 42)
+    assert ap.stoich == (1, -2, 3)
+    assert ap.args == (17, 42)
 
 
 def test_A():
