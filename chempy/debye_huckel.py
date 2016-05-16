e43c5c9240fbf7e13af7f51f34412315d957886d
Björn Dahlgren <bjodah@gmail.com>
2016-05-16 11:49:26 +0200

    Reintroduced debye_huckel for proper deprecation cycle

---
+++ chempy/debye_huckel.py
@@ -0,0 +1,12 @@
+from .util.pyutil import ChemPyDeprecationWarning
+
+import warnings
+
+from .electrolytes import (
+    A, B, limiting_log_gamma, extended_log_gamma, davies_log_gamma,
+    limiting_activity_product, extended_activity_product,
+    davies_activity_product
+)
+
+
+warnings.warn("use .electrolytes instead of .debye_huckel", ChemPyDeprecationWarning)
