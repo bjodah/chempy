c217be2d16c9b5f894a8fd9fd8b4ece9d12e1522
Björn Dahlgren <bjodah@gmail.com>
2016-11-18 15:34:32 +0100

    PEP440 compliant setup.py --version

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@
 import io
 from itertools import chain
 import os
+import re
 import shutil
+import subprocess
 import warnings
 
 from setuptools import setup
@@ -39,7 +41,15 @@ if len(RELEASE_VERSION) > 0:
 else:
     TAGGED_RELEASE = False
     # read __version__ attribute from _release.py:
-    exec(io.open(release_py_path, encoding='utf-8').read())
+    exec(open(release_py_path).read())
+    if __version__.endswith('git'):
+        try:
+            _git_version = subprocess.check_output(['git', 'describe']).rstrip().decode('utf-8')
+        except subprocess.CalledProcessError:
+            warnings.warn("A git-archive is being installed - version information incomplete.")
+        else:
+            warnings.warn("Using git to derive version: dev-branches may compete.")
+            __version__ = re.sub('v([0-9.]+)-(\d+)-(\w+)', r'\1.dev\2+\3', _git_version)
 
 submodules = [
     'chempy.electrochemistry',
