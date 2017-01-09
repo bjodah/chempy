f49ecaa6d43440477a1a9e264888cf2fcc71995a
Björn Dahlgren <bjodah@gmail.com>
2017-01-09 17:38:54 +0100

    Make test fail deterministically

--- chempy/kinetics/tests/test_ode.py
+++ chempy/kinetics/tests/test_ode.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import (absolute_import, division, print_function)
 
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
+from itertools import permutations
 import math
 
 import pytest
@@ -412,21 +413,23 @@ def test_get_odesys__max_euler_step_cb():
 
 @requires('numpy', 'pyodesys', 'sympy')
 def test_get_odesys__linear_dependencies():
-    rsys = ReactionSystem.from_string('\n'.join(['H2O -> H+ + OH-; 1e-4', 'OH- + H+ -> H2O; 1e10']))
-    odesys, extra = get_odesys(rsys)
+    for substances in permutations(['H2O', 'H+' , 'OH-']):
+        rsys = ReactionSystem.from_string('\n'.join(['H2O -> H+ + OH-; 1e-4', 'OH- + H+ -> H2O; 1e10']), substances)
+        assert isinstance(rsys.substances, OrderedDict)
+        odesys, extra = get_odesys(rsys)
 
-    af_H2O_H = extra['linear_dependencies'](['H2O', 'H+'])
-    import sympy
-    y0 = {k: sympy.Symbol(k+'0') for k in rsys.substances}
-    af_H2O_H(None, {odesys[k]: v for k, v in y0.items()}, None, sympy)  # ensure idempotent
-    exprs_H2O_H = af_H2O_H(None, {odesys[k]: v for k, v in y0.items()}, None, sympy)
-    ref_H2O_H = {
-        'H2O': y0['H2O'] + y0['OH-'] - odesys['OH-'],  # oxygen
-        'H+': 2*y0['H2O'] + y0['H+'] + y0['OH-'] - odesys['OH-'] - 2*(
-            y0['H2O'] + y0['OH-'] - odesys['OH-'])  # hydrogen
-    }
-    for k, v in ref_H2O_H.items():
-        assert (exprs_H2O_H[odesys[k]] - v) == 0
+        af_H2O_H = extra['linear_dependencies'](['H+', 'H2O'])
+        import sympy
+        y0 = {k: sympy.Symbol(k+'0') for k in rsys.substances}
+        af_H2O_H(None, {odesys[k]: v for k, v in y0.items()}, None, sympy)  # ensure idempotent
+        exprs_H2O_H = af_H2O_H(None, {odesys[k]: v for k, v in y0.items()}, None, sympy)
+        ref_H2O_H = {
+            'H2O': y0['H2O'] + y0['OH-'] - odesys['OH-'],  # oxygen
+            'H+': 2*y0['H2O'] + y0['H+'] + y0['OH-'] - odesys['OH-'] - 2*(
+                y0['H2O'] + y0['OH-'] - odesys['OH-'])  # hydrogen
+        }
+        for k, v in ref_H2O_H.items():
+            assert (exprs_H2O_H[odesys[k]] - v) == 0
 
 
 @requires('numpy', 'pyodesys', 'sympy', 'pycvodes')
