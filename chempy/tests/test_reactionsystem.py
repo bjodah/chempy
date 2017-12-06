3a7c1759aa45940f112f7d1aa31eac79d338a165
Björn Dahlgren <bjodah@gmail.com>
2017-12-06 20:12:08 +0100

    Add kwarg missing_substances_from_keys

--- chempy/tests/test_reactionsystem.py
+++ chempy/tests/test_reactionsystem.py
@@ -44,6 +44,15 @@ def test_ReactionSystem():
         rs['r2']
 
 
+def test_ReactionSystem__missing_substances_from_keys():
+    r1 = Reaction({'H2O'}, {'H+', 'OH-'})
+    with pytest.raises(ValueError):
+        ReactionSystem([r1], substances={'H2O': Substance.from_formula('H2O')})
+    kw = dict(missing_substances_from_keys=True, substance_factory=Substance.from_formula)
+    rs = ReactionSystem([r1], substances={'H2O': Substance.from_formula('H2O')}, **kw)
+    assert rs.substances['OH-'].composition == {0: -1, 1: 1, 8: 1}
+
+
 @requires(parsing_library)
 def test_ReactionSystem__check_balance():
     rs1 = ReactionSystem.from_string('\n'.join(['2 NH3 -> N2 + 3 H2', 'N2H4 -> N2 + 2 H2']))
