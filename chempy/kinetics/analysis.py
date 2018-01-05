1f472911ebe1050900eae48459c2c9ec3e3d2dcc
Björn Dahlgren <bjodah@gmail.com>
2018-01-05 01:40:12 +0100

    About to fix _kinetics_demo

--- chempy/kinetics/analysis.py
+++ chempy/kinetics/analysis.py
@@ -118,7 +118,8 @@ def dominant_reactions_graph(concs, rate_exprs_cb, rsys, substance_key, linthres
     from ..util.graph import rsys2graph
     rates = rate_exprs_cb(0, concs)
     eqk1, eqk2, eqs = _combine_rxns_to_eq(rsys) if combine_equilibria else ([], [], [])
-    rrate, rxns = zip(*_dominant_reaction_effects(substance_key, rsys, rates, linthreshy, eqk1, eqk2, eqs)[0])
+    rrate, rxns = zip(*_dominant_reaction_effects(
+        substance_key, rsys, rates, linthreshy, eqk1, eqk2, eqs)[0])
     rsys = ReactionSystem(rxns, rsys.substances, rsys.name)
     lg_rrate = np.log10(np.abs(rrate))
     rsys2graph(rsys, fname=fname, penwidths=1 + lg_rrate - np.min(lg_rrate), **kwargs)
