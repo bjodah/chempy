ad21bd67c9043ea268a0e475d4f45ea36f436159
Björn Dahlgren <bjodah@gmail.com>
2018-04-04 17:31:56 +0200

    Release docs for v0.6.7

---
+++ v0.6.7/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '0.6.7',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
