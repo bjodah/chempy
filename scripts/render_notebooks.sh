c45e673e0ce8edfb157e60f9f62882376fc8c3c8
Björn Dahlgren <bjodah@gmail.com>
2018-04-06 16:29:51 +0200

    Further tweak links in notebook rendering

--- scripts/render_notebooks.sh
+++ scripts/render_notebooks.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -e
+if [ -f index.ipynb ]; then
+    sed -i.bak0 's/ipynb/html/' index.ipynb
+    sed -i.bak1 's/filepath=index.html/filepath=index.ipynb/' index.ipynb  # mybinder link fix
+fi
 for dir in . examples/; do
     cd $dir
     jupyter nbconvert --debug --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 *.ipynb
     cd -
 done
-#../scripts/render_index.sh *.html
 
 cd examples/
 ../scripts/render_index.sh *.html
