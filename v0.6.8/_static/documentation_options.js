ce27d7028cd9f36096c0bc96fb1f06eff4390f9c
Björn Dahlgren <bjodah@gmail.com>
2018-08-12 18:53:52 +0200

    Release docs for v0.6.8

---
+++ v0.6.8/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '0.6.8',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
