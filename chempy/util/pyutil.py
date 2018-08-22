0203564f94e0b68a22ffb63a556c52907e9f0659
Björn Dahlgren <bjodah@gmail.com>
2018-08-22 17:31:12 +0200

    Add .util.pyutil.multi_indexed_cases

--- chempy/util/pyutil.py
+++ chempy/util/pyutil.py
@@ -4,8 +4,9 @@ General utilities and exceptions.
 """
 from __future__ import (absolute_import, division, print_function)
 
-from collections import defaultdict, namedtuple, Mapping
+from collections import defaultdict, namedtuple, Mapping, OrderedDict
 from functools import wraps
+from itertools import product
 import os
 import warnings
 
@@ -13,82 +14,8 @@ from .. import __url__
 from .deprecation import Deprecation
 
 
-def memoize(max_nargs=0):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args):
-            if max_nargs is not None and len(args) > max_nargs:
-                raise ValueError("memoization error")
-            if args not in wrapper.results:
-                wrapper.results[args] = func(*args)
-            return wrapper.results[args]
-        wrapper.results = {}
-        return wrapper
-    return decorator
-
-
-class defaultkeydict(defaultdict):
-    """ defaultdict where default_factory should have the signature key -> value
-
-    Examples
-    --------
-    >>> d = defaultkeydict(lambda k: '[%s]' % k, {'a': '[a]', 'b': '[B]'})
-    >>> d['a']
-    '[a]'
-    >>> d['b']
-    '[B]'
-    >>> d['c']
-    '[c]'
-
-    """
-
-    def __missing__(self, key):
-        if self.default_factory is None:
-            raise KeyError("Missing key: %s" % key)
-        else:
-            self[key] = self.default_factory(key)
-        return self[key]
-
-
-def defaultnamedtuple(typename, field_names, defaults=()):
-    """ Generates a new subclass of tuple with default values.
-
-    Parameters
-    ----------
-    typename : string
-        The name of the class.
-    field_names : str or iterable
-        An iterable of splitable string.
-    defaults : iterable
-        Default values for ``field_names``, counting ``[-len(defaults):]``.
-
-    Examples
-    --------
-    >>> Body = defaultnamedtuple('Body', 'x y z density', (1.0,))
-    >>> Body.__doc__
-    'Body(x, y, z, density)'
-    >>> b = Body(10, z=3, y=5)
-    >>> b._asdict()
-    OrderedDict([('x', 10), ('y', 5), ('z', 3), ('density', 1.0)])
-
-    Returns
-    -------
-    A new tuple subclass named ``typename``
-
-    """
-    Tuple = namedtuple(typename, field_names)
-    Tuple.__new__.__defaults__ = (None,) * len(Tuple._fields)
-    if isinstance(defaults, Mapping):
-        Tuple.__new__.__defaults__ = tuple(Tuple(**defaults))
-    else:
-        nmissing = len(Tuple._fields) - len(defaults)
-        defaults = (None,)*nmissing + tuple(defaults)
-        Tuple.__new__.__defaults__ = tuple(Tuple(*defaults))
-    return Tuple
-
-
 class NoConvergence(Exception):
-    pass  # why is this not in the Python standard library!?
+    pass
 
 
 class ChemPyDeprecationWarning(DeprecationWarning):
@@ -206,3 +133,114 @@ class AttributeContainer(object):
 
     def as_dict(self):
         return self.__dict__.copy()
+
+
+class defaultkeydict(defaultdict):
+    """ defaultdict where default_factory should have the signature key -> value
+
+    Examples
+    --------
+    >>> d = defaultkeydict(lambda k: '[%s]' % k, {'a': '[a]', 'b': '[B]'})
+    >>> d['a']
+    '[a]'
+    >>> d['b']
+    '[B]'
+    >>> d['c']
+    '[c]'
+
+    """
+
+    def __missing__(self, key):
+        if self.default_factory is None:
+            raise KeyError("Missing key: %s" % key)
+        else:
+            self[key] = self.default_factory(key)
+        return self[key]
+
+
+def defaultnamedtuple(typename, field_names, defaults=()):
+    """ Generates a new subclass of tuple with default values.
+
+    Parameters
+    ----------
+    typename : string
+        The name of the class.
+    field_names : str or iterable
+        An iterable of splitable string.
+    defaults : iterable
+        Default values for ``field_names``, counting ``[-len(defaults):]``.
+
+    Examples
+    --------
+    >>> Body = defaultnamedtuple('Body', 'x y z density', (1.0,))
+    >>> Body.__doc__
+    'Body(x, y, z, density)'
+    >>> b = Body(10, z=3, y=5)
+    >>> b._asdict()
+    OrderedDict([('x', 10), ('y', 5), ('z', 3), ('density', 1.0)])
+
+    Returns
+    -------
+    A new tuple subclass named ``typename``
+
+    """
+    Tuple = namedtuple(typename, field_names)
+    Tuple.__new__.__defaults__ = (None,) * len(Tuple._fields)
+    if isinstance(defaults, Mapping):
+        Tuple.__new__.__defaults__ = tuple(Tuple(**defaults))
+    else:
+        nmissing = len(Tuple._fields) - len(defaults)
+        defaults = (None,)*nmissing + tuple(defaults)
+        Tuple.__new__.__defaults__ = tuple(Tuple(*defaults))
+    return Tuple
+
+
+def multi_indexed_cases(od):
+    """ Returns a list of length-2 tuples
+
+    Each tuple consist of a multi-index (tuple of integers) and a dictionary.
+
+    Parameters
+    ----------
+    od : OrderedDict
+        Maps each key to a number of values. ``list`` and ``tuple`` instances
+        are converted to ``OrderedDict``.
+
+    Examples
+    --------
+    >>> from collections import OrderedDict
+    >>> od = OrderedDict([('a', [1, 2, 3], 'b': [False, True])])
+    >>> cases = multi_indiexed_cases(od)
+    >>> len(cases)
+    6
+    >>> midxs, case_kws = zip(*cases)
+    >>> midxs[0]
+    (0, 0)
+    >>> case_kws[0] == {'a': 1, 'b': False}
+    True
+
+    Returns
+    -------
+    List of length-2 tuples, each consisting of one tuple of inidices and one dictionary.
+
+    """
+    if isinstance(od, (list, tuple)):
+        od = OrderedDict(od)
+    if not isinstance(od, OrderedDict):
+        raise NotImplementedError("Expected an OrderedDcit")
+    keys, values = map(list, [od.keys(), od.values()])
+    return [(mi, {k: v[i] for k, v, i in zip(keys, values, mi)}) for mi in product(*map(range, map(len, values)))]
+
+
+def memoize(max_nargs=0):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args):
+            if max_nargs is not None and len(args) > max_nargs:
+                raise ValueError("memoization error")
+            if args not in wrapper.results:
+                wrapper.results[args] = func(*args)
+            return wrapper.results[args]
+        wrapper.results = {}
+        return wrapper
+    return decorator
