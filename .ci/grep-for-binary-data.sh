4d693dc8bca50a67c353cbf4e091ff80e1072243
Björn Dahlgren <bjodah@gmail.com>
2018-08-30 17:56:23 +0200

    Tweak drone setup

--- .ci/grep-for-binary-data.sh
+++ .ci/grep-for-binary-data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if grep '"image/png":' -R . --include "*.ipynb" --exclude "./ci_cache/*"; then
+if git grep '"image/png":' -- '*.ipynb'; then
     >&2 echo "You may not check in binary data into the repository."
     >&2 echo "See e.g. .jupyter/jupyter_notebook_config.py for how to exclude output."
     exit 1
