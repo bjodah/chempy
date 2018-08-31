c0d62c955f6ed0fec404877a90ae04f839b6df05
Björn Dahlgren <bjodah@gmail.com>
2018-08-31 13:44:02 +0200

    secret not in pr

--- scripts/render_notebook.sh
+++ scripts/render_notebook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-jupyter nbconvert --debug --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 $1 >$1.log
+jupyter nbconvert --debug --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 $1 2>&1 >$1.log
 if [[ $? -ne 0 ]]; then
     2>&1 echo "Failed to render $1"
     cat $1.log
