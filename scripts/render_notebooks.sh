02bec40b1d6e3156351c99ef1a7c81ad3002c19f
Björn Dahlgren <bjodah@gmail.com>
2018-08-31 13:14:35 +0200

    Tweak parallel render

--- scripts/render_notebooks.sh
+++ scripts/render_notebooks.sh
@@ -3,9 +3,10 @@ if [ -f index.ipynb ]; then
     sed -i.bak0 's/ipynb/html/' index.ipynb
     sed -i.bak1 's/filepath=index.html/filepath=index.ipynb/' index.ipynb  # mybinder link fix
 fi
+SCRIPTS_PATH=$(unset CDPATH && cd "$(dirname "$0")" && echo $PWD)
 for dir in . examples/; do
     cd $dir
-    find . -iname "*.ipynb" | xargs -P 4 ./scripts/render_notebook.sh
+    find . -iname "*.ipynb" | xargs -P 4 "$SCRIPTS_PATH/render_notebook.sh"
     cd -
 done
 
