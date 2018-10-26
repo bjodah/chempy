1ad5e6672469742dd631a0e6cf776ad61aec7089
Björn Dahlgren <bjodah@gmail.com>
2018-10-26 16:21:51 +0200

    req. pyneqsys-0.5.5 (avoid flood of deprecation warnings)

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ setup_kwargs = dict(
     classifiers=classifiers,
     install_requires=[
         'numpy>1.11.3', 'scipy>=1.0.1', 'matplotlib>=2.2.3',
-        'sympy>=1.1.1,!=1.2', 'quantities>=0.12.1', 'pyneqsys>=0.5.4',
+        'sympy>=1.1.1,!=1.2', 'quantities>=0.12.1', 'pyneqsys>=0.5.5',
         'pyodesys>=0.12.4' if sys.version_info[0] >= 3 else 'pyodesys<0.12',
         'pyparsing>=2.0.3', 'sym>=0.3.4', 'jupyter',
         'pulp>=1.6.8',
