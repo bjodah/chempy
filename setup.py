0cb93ecb2a02ce8762ccd2623b215fcf2f45e242
Björn Dahlgren <bjodah@gmail.com>
2018-10-27 16:08:57 +0200

    req. pyneqsys-0.5.6

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ setup_kwargs = dict(
     classifiers=classifiers,
     install_requires=[
         'numpy>1.11.3', 'scipy>=1.0.1', 'matplotlib>=2.2.3',
-        'sympy>=1.1.1,!=1.2', 'quantities>=0.12.1', 'pyneqsys>=0.5.5',
+        'sympy>=1.1.1,!=1.2', 'quantities>=0.12.1', 'pyneqsys>=0.5.6',
         'pyodesys>=0.11.16,<0.12.0', 'pyparsing>=2.0.3', 'sym>=0.3.3', 'jupyter',
         'pulp>=1.6.8',
         # 'dot2tex>=2.9.0'
