193e3a6710aa01e1bf466ec42e848e87f686c5b2
Björn Dahlgren <bjodah@gmail.com>
2019-03-01 10:22:44 +0100

    Add .properties.gas_sol_electrolytes_schumpe_1993

---
+++ chempy/properties/tests/test_gas_sol_electrolytes_schumpe_1993.py
@@ -0,0 +1,6 @@
+from chempy.units import default_units as u
+from ..gas_sol_electrolytes_schumpe_1993 import lg_solubility_ratio
+
+
+def test_lg_solubility_ratio():
+    lgr = lg_solubility_ratio({'Br-': 0.05*u.molar, 'Na+': 0.050*u.molar}, 'N2O', units=u)
