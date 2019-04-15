daf87772b27e8d565f24042f4ead54b34d857be5
Björn Dahlgren <bjodah@gmail.com>
2019-04-15 16:38:31 +0200

    Try less verbose notebook rendering script

--- scripts/render_notebooks.sh
+++ scripts/render_notebooks.sh
@@ -1,11 +1,30 @@
 #!/bin/bash -e
+function quiet_unless_fail {
+    # suppresses function output unless exit status is != 0
+    OUTPUT_FILE=$(tempfile)
+    #/bin/rm --force /tmp/suppress.out 2>/dev/null
+    EXECMD=${1+"$@"}
+    $EXECMD > ${OUTPUT_FILE} 2>&1
+    EXIT_CODE=$?
+    if [ ${EXIT_CODE} -ne 0 ]; then
+	cat ${OUTPUT_FILE}
+	echo "The following command exited with exit status ${EXIT_CODE}: ${EXECMD}"
+	/bin/rm ${OUTPUT_FILE}
+	exit $?
+    fi
+    /bin/rm ${OUTPUT_FILE}
+}
+
 if [ -f index.ipynb ]; then
     sed -i.bak0 's/ipynb/html/' index.ipynb
     sed -i.bak1 's/filepath=index.html/filepath=index.ipynb/' index.ipynb  # mybinder link fix
 fi
 for dir in . examples/; do
     cd $dir
-    jupyter nbconvert --debug --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 *.ipynb
+    for fname in *.ipynb; do
+        echo "rendering ${fname}..."
+        quiet_unless_fail jupyter nbconvert --debug --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 ${fname}
+    done
     cd -
 done
 
