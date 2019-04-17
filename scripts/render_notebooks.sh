b2274b79546369d01cd31a9c33b49924ead5dab0
Björn Dahlgren <bjodah@gmail.com>
2019-04-17 17:41:02 +0200

    [WIP] Added notebooks debugging units/Expr interaction (#146)
    * [WIP] Added notebooks debugging units/Expr interaction
    
    * Try less verbose notebook rendering script
    
    * subshell +e
    
    * fix +e/-e
    
    * tweak exit cond.
    
    * Fix rendering script
    
    * Minor fixes

--- scripts/render_notebooks.sh
+++ scripts/render_notebooks.sh
@@ -1,13 +1,35 @@
 #!/bin/bash -e
+function quiet_unless_fail {
+    # suppresses function output unless exit status is != 0
+    OUTPUT_FILE=$(tempfile)
+    #/bin/rm --force /tmp/suppress.out 2>/dev/null
+    EXECMD=${1+"$@"}
+    $EXECMD > ${OUTPUT_FILE} 2>&1
+    EXIT_CODE=$?
+    if [ ${EXIT_CODE} -ne 0 ]; then
+	cat ${OUTPUT_FILE}
+	echo "The following command exited with exit status ${EXIT_CODE}: ${EXECMD}"
+	/bin/rm ${OUTPUT_FILE}
+    fi
+    /bin/rm ${OUTPUT_FILE}
+}
+
 if [ -f index.ipynb ]; then
     sed -i.bak0 's/ipynb/html/' index.ipynb
     sed -i.bak1 's/filepath=index.html/filepath=index.ipynb/' index.ipynb  # mybinder link fix
 fi
+set +e
 for dir in . examples/; do
     cd $dir
-    jupyter nbconvert --debug --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 *.ipynb
+    for fname in *.ipynb; do
+        echo "rendering ${fname}..."
+        quiet_unless_fail jupyter nbconvert --debug --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 "${fname}"
+        if [ ${EXIT_CODE} -ne 0 ]; then
+            exit ${EXIT_CODE}
+        fi
+    done
     cd -
 done
-
+set -e
 cd examples/
 ../scripts/render_index.sh *.html
