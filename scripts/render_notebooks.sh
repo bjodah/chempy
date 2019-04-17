ca357a5cfc4f610e2114d3182a8489ae55e56093
Björn Dahlgren <bjodah@gmail.com>
2019-04-17 13:07:32 +0200

    fix +e/-e

--- scripts/render_notebooks.sh
+++ scripts/render_notebooks.sh
@@ -19,14 +19,15 @@ if [ -f index.ipynb ]; then
     sed -i.bak0 's/ipynb/html/' index.ipynb
     sed -i.bak1 's/filepath=index.html/filepath=index.ipynb/' index.ipynb  # mybinder link fix
 fi
+set +e
 for dir in . examples/; do
     cd $dir
     for fname in *.ipynb; do
         echo "rendering ${fname}..."
-        ( set +e; quiet_unless_fail jupyter nbconvert --debug --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 "${fname}" )
+        quiet_unless_fail jupyter nbconvert --debug --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 "${fname}"
     done
     cd -
 done
-
+set -e
 cd examples/
 ../scripts/render_index.sh *.html
