98bb95ab4d951180f76e5c48503d369a8363d378
spizwhiz <spencerwhitman88@gmail.com>
2020-07-20 16:03:11 -0700

    Update parsing.py

--- chempy/util/parsing.py
+++ chempy/util/parsing.py
@@ -266,7 +266,7 @@ def formula_to_composition(formula, prefixes=None,
     True
     >>> formula_to_composition('.NHO-(aq)') == {0: -1, 1: 1, 7: 1, 8: 1}
     True
-    >>> formula_to_composition('Na2CO3.7H2O') == {11: 2, 6: 1, 8: 10, 1: 14}
+    >>> formula_to_composition('Na2CO3:7H2O') == {11: 2, 6: 1, 8: 10, 1: 14}
     True
 
     """
