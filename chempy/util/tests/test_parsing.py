7a679322fa660f57262bcc9898dd1c5d2343b1a1
spizwhiz <spencerwhitman88@gmail.com>
2020-07-20 17:08:39 -0700

    Update test_parsing.py

--- chempy/util/tests/test_parsing.py
+++ chempy/util/tests/test_parsing.py
@@ -123,12 +123,12 @@ def test_formula_to_latex():
     assert formula_to_latex('NaCl(s)') == 'NaCl(s)'
     assert formula_to_latex('e-(aq)') == 'e^{-}(aq)'
     assert formula_to_latex('Ca+2(aq)') == 'Ca^{2+}(aq)'
-    assert formula_to_latex(':NO2(g)') == r'\colon NO_{2}(g)'
-    assert formula_to_latex(':NH2') == r'\colon NH_{2}'
+    assert formula_to_latex(':NO2(g)') == r'^\colon NO_{2}(g)'
+    assert formula_to_latex(':NH2') == r'^\colon NH_{2}'
     assert formula_to_latex('ONOOH') == 'ONOOH'
-    assert formula_to_latex(':ONOO') == r'\colon ONOO'
-    assert formula_to_latex(':NO3/2-') == r'\colon NO_{3}^{2-}'
-    assert formula_to_latex(':NO3-2') == r'\colon NO_{3}^{2-}'
+    assert formula_to_latex(':ONOO') == r'^\colon ONOO'
+    assert formula_to_latex(':NO3/2-') == r'^\colon NO_{3}^{2-}'
+    assert formula_to_latex(':NO3-2') == r'^\colon NO_{3}^{2-}'
     assert formula_to_latex('alpha-FeOOH(s)') == r'\alpha-FeOOH(s)'
     assert formula_to_latex('epsilon-Zn(OH)2(s)') == (
         r'\varepsilon-Zn(OH)_{2}(s)')
