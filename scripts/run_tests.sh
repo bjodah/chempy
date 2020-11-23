dceedb4c494707e3fef736c92ef6993480c69f7b
Björn Ingvar Dahlgren <bjodah@gmail.com>
2020-11-24 00:31:28 +0100

    About to tag v0.8.0

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,6 +3,6 @@
 #   $ ./scripts/run_tests.sh
 # or
 #   $ ./scripts/run_tests.sh --cov chempy --cov-report html
-CHEMPY_DEPRECATION_FILTER='ignore' ${PYTHON:-python3} -m pytest -ra --doctest-modules --pep8 --flakes $@
+CHEMPY_DEPRECATION_FILTER='ignore' ${PYTHON:-python3} -m pytest -ra --doctest-modules --flakes $@
 MPLBACKEND=Agg ${PYTHON:-python3} -m doctest README.rst
 rstcheck README.rst
