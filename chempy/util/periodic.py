8553bebec1f865f9b5d9a51849661ef2851873b3
Chris Porch <cbporch@gmail.com>
2021-09-25 11:34:52 -0400

    Let atomic_number accept lowercase symbols
    - Add docstring

--- chempy/util/periodic.py
+++ chempy/util/periodic.py
@@ -140,8 +140,20 @@ groups[18] = accum_period_lengths  # noble gases
 
 
 def atomic_number(name):
+    """Provide atomic number for a given element
+
+    Parameters
+    ----------
+    name: str
+        Full name or chemical symbol of an element
+
+    Returns
+    -------
+    int
+        Atomic number
+    """
     try:
-        return symbols.index(name) + 1
+        return symbols.index(name.capitalize()) + 1
     except ValueError:
         return lower_names.index(name.lower()) + 1
 
--- chempy/util/tests/test_periodic.py
+++ chempy/util/tests/test_periodic.py
@@ -15,6 +15,7 @@ from ..parsing import formula_to_composition, parsing_library
 
 def test_atomic_number():
     assert atomic_number("U") == 92
+    assert atomic_number("u") == 92
     assert atomic_number("carbon") == 6
     assert atomic_number("oganesson") == 118
     with pytest.raises(ValueError):
