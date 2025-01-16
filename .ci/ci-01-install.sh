fe40c9d7e6f032026549ae666f13e903e71a7bbf
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-01-16 10:29:25 +0100

    ci: install dev version of 'sym'

--- .ci/ci-01-install.sh
+++ .ci/ci-01-install.sh
@@ -6,8 +6,11 @@ export LD_LIBRARY_PATH=$SUNDBASE/lib
 source /opt-3/cpython-v3.*-apt-deb/bin/activate
 
 INSTALL_PIP_FLAGS="--cache-dir $CACHE_ROOT/pip_cache ${INSTALL_PIP_FLAGS:-}"  # --user
-for pypkg in pyodeint pygslodeiv2 pycompilation pycodeexport pycvodes pyodesys; do
+for pypkg in pyodeint pygslodeiv2 pycompilation pycodeexport pycvodes sym pyodesys; do
     case $pypkg in
+        sym)
+            pypkg_fqn="git+https://github.com/bjodah/sym@jun21#egg=sym"
+            ;;
         pyodeint)
             pypkg_fqn="git+https://github.com/bjodah/pyodeint@sep21#egg=pyodeint"
             ;;
@@ -30,19 +33,12 @@ for pypkg in pyodeint pygslodeiv2 pycompilation pycodeexport pycvodes pyodesys; 
             pypkg_fqn=$pypkg
             ;;
     esac
-    # if [[ $pypkg == "pycvodes" ]]; then
-    #     env \
-    #         CFLAGS="-isystem $SUNDBASE/include ${CFLAGS:-}" \
-    #         LDFLAGS="-Wl,--disable-new-dtags -Wl,-rpath,$SUNDBASE/lib -L$SUNDBASE/lib ${LDFLAGS:-}" \
-    #         python -m pip install $INSTALL_PIP_FLAGS $pypkg_fqn
-    # else
-        python -m pip install $INSTALL_PIP_FLAGS $pypkg_fqn
-    # fi
-    ( cd /tmp; python -m pytest -k "not pool_discontinuity_approx" --pyargs $pypkg )
+    python -m pip install $INSTALL_PIP_FLAGS $pypkg_fqn
+    #( cd /tmp; python -m pytest -k "not pool_discontinuity_approx" --pyargs $pypkg )
 done
 
 python3 -m pip install $INSTALL_PIP_FLAGS -e .[all]
-python3 -c "import pycvodes; import pyodesys; import pygslodeiv2"  # debug this CI config
+python3 -c "import pycvodes; import pyodesys; import pygslodeiv2"
 git fetch -tq
 python3 setup.py sdist                    # test pip installable sdist (checks MANIFEST.in)
 git archive -o dist/chempy-head.zip HEAD  # test pip installable zip (symlinks break)
