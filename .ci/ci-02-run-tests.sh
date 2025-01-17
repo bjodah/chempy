cba1a43eabc95f55950d118fb9b5aef0d24ec13a
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-01-17 14:38:30 +0100

    ci: set paths

--- .ci/ci-02-run-tests.sh
+++ .ci/ci-02-run-tests.sh
@@ -3,5 +3,9 @@ set -euxo pipefail
 source /opt-3/cpython-v3.*-apt-deb/bin/activate
 
 [[ $(python3 setup.py --version) =~ ^[0-9]+.* ]]
+export \
+    PYODESYS_CVODE_FLAGS="-isystem $SUNDBASE/include -isystem /usr/include/suitesparse" \
+    PYODESYS_CVODE_LDFLAGS="-Wl,--disable-new-dtags -Wl,-rpath,$SUNDBASE/lib -L$SUNDBASE/lib"
+
 ./scripts/run_tests.sh --cov chempy --cov-report html
 ./scripts/coverage_badge.py htmlcov/ htmlcov/coverage.svg
