a646211fb46a7c8cfd5e33f1696e2b0e2c890862
Charles Weiss <charles.weiss@augie.edu>
2025-08-14 16:26:56 -0500

    Updated data tables and tests
    Fixed a few typos in tables.py, reformatted the dictionaries for easier reading/proof reading, and added more comments and a reference. Added more tests to test_representations.py to cover a wider set of point groups along with more tests for generating reducible representations from stationary atoms.

--- chempy/symmetry/tests/test_representations.py
+++ chempy/symmetry/tests/test_representations.py
@@ -5,37 +5,56 @@ import numpy as np
 from ..representations import Reducible
 
 # Test data with (reducible representation, num of irreducibles, point group)
-test_data = (
-            ((2), (2), 'c1'),
+reducible_data = (
             ((2, 0), (1, 1), 'cs'),
             ((3, 1), (2, 1), 'ci'),
+            ((2), (2), 'c1'),
             ((3, 1), (2, 1), 'c2'),
             ((3, 0, 0), (1, 1), 'C3'),
             ((4, 0, 0, 0), (1, 1, 1), 'c4'),
             ((21, 1, 1, 1, 1), (5, 4, 4), 'c5'),
+            ((6, 0, 0, 0, 0, 0), (1, 1, 1, 1), 'c6'),
             ([9, -1, 3, 1], [3, 1, 3, 2], 'c2v'),
             ([3, 0, 1], (1, 0, 1), 'c3v'),
             ((6, 0, 2, 0, 0), (1, 1, 1, 1, 1), 'c4v'),
             ((4, 0, 2, 2), (2, 1, 0, 1), 'c2h'),
-            ([15, 0, 0, 7, -2, -2], [3, 4, 2, 1], 'c3h'),
+            ((15, 0, 0, 7, -2, -2), (3, 4, 2, 1), 'c3h'),
+            ((5, -1, 1, -1, -1, 1, -5, 1), (0, 0, 1, 1, 2, 0), 'c4h'),
             ((4, 0, 0, 0), (1, 1, 1, 1), 'd2'),
-            ([4, 1, 0, 0, 2], (1, 0, 0, 0, 1), 'Td'),
-            ([4, 0, 0, 2, 0, 0, 0, 4, 2, 0],
-             (1, 0, 1, 0, 0, 0, 0, 0, 0, 1), 'd4h'),
+            ((5, -1, 1, 3, -1), (1, 0, 2, 0, 1), 'd2d'),
             ((5, 2, 1, 3, 0, 3), (2, 0, 1, 0, 1, 0), 'd3h'),
-            ((6, 0, 0, 0, -2, 0, 0, 0, 0, -6, 0, 2),
-             (0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1), 'd6h'),
-            ([6, 0, 0, 2, 2, 0, 0, 0, 4, 2],
-             (1, 0, 1, 0, 0, 0, 0, 0, 1, 0), 'Oh'),
-            ((6, 0, 0, 0, 0, 0), (1, 1, 1, 1), 'c6')
+            ((4, 0, 0, 2, 0, 0, 0, 4, 2, 0), (1, 0, 1, 0, 0, 0, 0, 0, 0, 1), 'd4h'),
+            ((6, 0, 0, 0, -2, 0, 0, 0, 0, -6, 0, 2), (0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1), 'd6h'),
+            ([4, 1, 0, 0, 2], (1, 0, 0, 0, 1), 'Td'),
+            ([8, -1, 4, 0, -2], (0, 1, 2, 1, 0), 'td'),
+            ((6, 0, 0, 2, 2, 0, 0, 0, 4, 2), (1, 0, 1, 0, 0, 0, 0, 0, 1, 0), 'Oh'),
+            ((7, 1, 1, 3, 3, 1, 1, 1, 5, 3), (2, 0, 1, 0, 0, 0, 0, 0, 1, 0), 'oh'),
             )
 
 
-@pytest.mark.parametrize('gamma, true_n_irred, group', test_data)
+@pytest.mark.parametrize('gamma, true_n_irred, group', reducible_data)
 def test_decomp(gamma, true_n_irred, group):
     calc_n_irred = Reducible(gamma, group).decomp()
 
     assert np.all(true_n_irred == calc_n_irred)
+
+all_motion_data = (
+    ((6, 0, 0, 6), (18, 0, 0, 6), 'c2h'),
+    ((3, 1, 3, 1), (9, -1, 3, 1), 'c2v'),
+    ((4, 1, 2), (12, 0, 2), 'c3v'),
+    ((6, 2, 2, 4, 2), (18, 2, -2, 4, 2), 'c4v'),
+    ((8, 2, 0, 0, 0, 4), (24, 0, 0, 0, 0, 4), 'd3d'),
+    ((6, 3, 2, 4, 1, 4), (18, 0, -2, 4, -2, 4), 'd3h'),
+    ((5, 1, 1, 3, 1, 1, 1, 5, 3, 1), (15, 1, -1, -3, -1, -3, -1, 5, 3, 1), 'd4h'),
+    ((5, 2, 1, 1, 3), (15, 0, -1, -1, 3), 'td'),
+    ((7, 1, 1, 3, 3, 1, 1, 1, 5, 3), (21, 0, -1, 3, -3, -3, -1, 0, 5, 3), 'oh')
+    )
+
+@pytest.mark.parametrize('stationary_atoms, reducible_rep, group', all_motion_data)
+def test_from_atoms(stationary_atoms, reducible_rep, group):
+
+    assert np.all(Reducible.from_atoms(stationary_atoms, group).gamma ==
+                  np.array(reducible_rep))
 
 
 class Test_ReducibleMethods():
@@ -55,7 +74,6 @@ class Test_ReducibleMethods():
         assert np.all(pCOBrMn.decomp() == np.array([2, 0, 1, 0, 1]))
         assert np.all(pCOBrMn.decomp(to_dict=True) ==
                       {'A1': 2, 'A2': 0, 'B1': 1, 'B2': 0, 'E': 1})
-
 
     def test_vibe(self):
         water = Reducible([9, -1, 3, 1], 'c2v', all_motion=True)
@@ -79,7 +97,6 @@ class Test_ReducibleMethods():
         assert np.all(water.ir_active(to_dict=True) ==
                       {'A1': 2, 'A2': 0, 'B1': 1, 'B2': 0})
 
-
     def test_raman(self):
         water = Reducible([9, -1, 3, 1], 'c2v', all_motion=True)
         tDCE = Reducible([18, 0, 0, 6], 'c2h', all_motion=True)
@@ -91,16 +108,6 @@ class Test_ReducibleMethods():
         assert np.all(tDCE.raman_active(to_dict=True) ==
                       {'Ag': 5, 'Bg': 1, 'Au': 0, 'Bu': 0})
 
-
-    def test_from_atoms(self):
-        assert np.all(Reducible.from_atoms([6, 0, 0, 6], 'c2h').gamma ==
-                      np.array([18, 0, 0, 6]))
-
-        assert np.all(Reducible.from_atoms([3, 1, 3, 1], 'c2v').gamma ==
-                      np.array([9, -1, 3, 1]))
-
-        assert np.all(Reducible.from_atoms([21, 1, 1, 1, 1, 1, 1, 5], 'd5h').
-                      gamma == np.array([63, 3, -1, -1, 1, 1, -2, 5]))
 
     def test_from_irred(self):
         test_rep = Reducible.from_irred([1, 0, 1, 0], 'c2v')
