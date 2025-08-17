a6c1497ada89ee2cfeb1554842fa5712c7669fa9
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-08-17 18:53:41 +0200

    Drop --user flag from pip --install

--- .ci/run-01-install-deps-create-sdist.sh
+++ .ci/run-01-install-deps-create-sdist.sh
@@ -3,8 +3,8 @@ set -euxo pipefail
 . .ci/_source_common_env.sh
 if [ ! -d $PYTHONUSERBASE ]; then mkdir -p $PYTHONUSERBASE; fi
 
-python -m pip install --cache-dir $CACHE_ROOT/pip_cache --user --upgrade-strategy=eager --upgrade cython
-python -m pip install --cache-dir $CACHE_ROOT/pip_cache --user -e .[all]
+python -m pip install --cache-dir $CACHE_ROOT/pip_cache --upgrade-strategy=eager --upgrade cython
+python -m pip install --cache-dir $CACHE_ROOT/pip_cache -e .[all]
 python -c "import pycvodes; import pyodesys; import pygslodeiv2"  # debug this CI config
 git fetch -tq
 python setup.py sdist                    # test pip installable sdist (checks MANIFEST.in)
