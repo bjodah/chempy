717b0fce92fb5b36b53f1eca16f0a8858d68fe41
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-08-17 18:49:33 +0200

    Activate virtual environment in CI-image

--- .ci/_source_common_env.sh
+++ .ci/_source_common_env.sh
@@ -4,3 +4,5 @@ export CPATH=$SUNDIALS_ROOT/include:/usr/include/suitesparse:${CPATH:-}  # sunli
 export CPLUS_INCLUDE_PATH=${Boost_ROOT}/include
 export LIBRARY_PATH=$SUNDIALS_ROOT/lib
 export LD_LIBRARY_PATH=$SUNDIALS_ROOT/lib
+
+source $(compgen -G "/opt-3/cpython-v3.*-apt-deb/bin/activate")
