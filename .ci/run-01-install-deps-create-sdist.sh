3d3317e2c4be1f4c7a6939797bb9b73bca07daf6
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-08-25 10:31:26 +0200

    Try using pyproject.toml for sdist

--- .ci/run-01-install-deps-create-sdist.sh
+++ .ci/run-01-install-deps-create-sdist.sh
@@ -7,5 +7,6 @@ python -m pip install --cache-dir $CACHE_ROOT/pip_cache --upgrade-strategy=eager
 python -m pip install --cache-dir $CACHE_ROOT/pip_cache -e .[all]
 python -c "import pycvodes; import pyodesys; import pygslodeiv2"  # debug this CI config
 git fetch -tq
-python setup.py sdist                    # test pip installable sdist (checks MANIFEST.in)
+#python setup.py sdist                    # test pip installable sdist (checks MANIFEST.in)
+python -m build --sdist                    # test pip installable sdist (checks MANIFEST.in)
 git archive -o dist/chempy-head.zip HEAD  # test pip installable zip (symlinks break)
